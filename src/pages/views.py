3c16884f004dfbbab7b3e74814b95ccd61e1dc2c
soorajanilkumar <soorajanilkumar@gmail.com>
2020-12-09 18:12:20 +0100

    add contacts page

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(*args, **kwargs):
 	return HttpResponse("<h1>Hello World </h1>")
+
+def contact_view(*args, **kwargs):
+	return HttpResponse("<h1>Contacts page </h1>")
