c2b7cec1fb106664688854c26786ab39b004c1f1
soorajanilkumar <soorajanilkumar@gmail.com>
2020-12-09 17:20:22 +0100

    add a product app and define a model

--- src/trydjango/settings.py
+++ src/trydjango/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third party apps
+
+    # Own apps
+    'products',
 ]
 
 MIDDLEWARE = [
