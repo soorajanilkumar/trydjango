613a2df81242ff17460ded253e07381f46821d17
soorajanilkumar <soorajanilkumar@gmail.com>
2020-12-10 11:55:20 +0100

    add template for home

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,8 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-	return HttpResponse("<h1>Hello World </h1>")
+def home_view(request, *args, **kwargs):
+	print(args, kwargs)
+	print(request.user)
+	# return HttpResponse("<h1>Hello World </h1>")
+	return render(request,"home.html",{})
 
 def contact_view(*args, **kwargs):
 	return HttpResponse("<h1>Contacts page </h1>")
